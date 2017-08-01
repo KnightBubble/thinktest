8a0df95656017ec1352e4bb1a1e428b60534b87a
huanz <260540233@qq.com>
2017-08-01 22:12:27 +0800

    lean cloud

--- src/common/config/env/development.js
+++ src/common/config/env/development.js
@@ -1,7 +1,8 @@
 'use strict';
 
 export default {
-    host: 'http://serve.leanapp.cn',
+    port: parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000),
+    url: 'http://serve.leanapp.cn',
     wechat: {
         appid: 'wxcb816a269be9eebe',
         appsecret: '6bacd2cdc8344530629f5a420a86e518'
