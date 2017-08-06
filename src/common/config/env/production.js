34ef2f40c5c32ef0364f3882ed674a742c41f938
bukas <yhz1219@gmail.com>
2017-08-06 16:04:43 +0800

    mod config

--- src/common/config/env/production.js
+++ src/common/config/env/production.js
@@ -1,17 +1,12 @@
 'use strict';
 
 export default {
-    resource_on: false,
-    // wechat: {
-    //     appid: 'wx580c5fee09078215',
-    //     appsecret: 'ddc46a6eafa7fcbc075efd5a649d2205'
-    // }
     port: 80,
-    url: 'http://serve.leanapp.cn',
     wechat: {
-        appid: 'wxcb816a269be9eebe',
-        appsecret: '6bacd2cdc8344530629f5a420a86e518'
+        appid: 'wx580c5fee09078215',
+        appsecret: 'ddc46a6eafa7fcbc075efd5a649d2205'
     },
+    url: 'http://www.xjzxtf.com',
     sms: {
         accessKeyId: 'LTAIRi3VHqrhqyWs',
         secretAccessKey: 'tTvS4Oxn3XnKYzwUAriDb7VSvwhUsN',
