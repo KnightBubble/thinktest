d661c69cf06be98d93d4b7a52ea52098d6970861
huanz <260540233@qq.com>
2017-08-12 15:20:30 +0800

    add jsconfig

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1,26 +1,21 @@
 'use strict';
 import fs from 'fs';
 import path from 'path';
-import {
-    promisify
-} from 'util';
-import Base from './base.js';
 import OAuth from 'co-wechat-oauth';
+import API from 'co-wechat-api';
 
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
+import Base from './base.js';
 
 const wechatConf = think.config('wechat');
-const WechatOAuthApi = new OAuth(wechatConf.appid, wechatConf.appsecret, async function (openid) {
-    // 传入一个根据openid获取对应的全局token的方法
-    //think.cache('name', 'value');
-    let token = await think.cache(openid);
-    return token;
-}, async function (openid, token) {
-    // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
-    // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
-    // 持久化时请注意，每个openid都对应一个唯一的token!
+const WechatOAuthApi = new OAuth(wechatConf.appid, wechatConf.appsecret, async(openid) => {
+    return await think.cache(openid);
+}, async(openid, token) => {
     await think.cache(openid, token);
+});
+const WechatJSApi = new API(wechatConf.appid, wechatConf.appsecret, async() => {
+    return await think.cache('access_token');
+}, async(token) => {
+    await think.cache('access_token', token);
 });
 
 export default class extends Base {
@@ -360,5 +355,10 @@ export default class extends Base {
 
             });
         });
+    }
+    async jsconfigAction() {
+        let params = this.post();
+        let res = WechatJSApi.getJsConfig(params);
+        return this.json(res);
     }
 }
